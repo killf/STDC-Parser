6f7a563dbf22eac06390c48da86fca6454e5165f
Ren Peng <renpeng@xiaoice.com>
2021-09-07 19:46:30 +0800

    update

--- solver.py
+++ solver.py
@@ -17,7 +17,7 @@ class Solver:
         self.device = torch.device(cfg.device)
         self.output_dir = cfg.output_dir
 
-        train_transform = Compose([ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5), RandomScale((0.75, 1.25)), RandomCrop(448), ToTensor(), Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+        train_transform = Compose([ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5), RandomScale((0.75, 1.25)), RandomCrop(cfg.crop_size), ToTensor(), Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
         train_data = DATASETS[cfg.dataset](cfg.data_dir, file_list="train.txt", image_size=cfg.image_size, transform=train_transform)
         self.train_loader = DataLoader(train_data, cfg.batch_size, True, num_workers=4)
 
